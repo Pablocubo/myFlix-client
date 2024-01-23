50b906c0c3781859b0b1d1218bcb075d74cd0b5a
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2024-01-23 16:43:57 +0100

    addes login view, logout button, from validation

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,60 @@
+import  { useState } from "react";
+
+export const LoginView = ({onLoggedIn}) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password,
+    };
+
+    fetch("https://letflix-0d183cd4a94e.herokuapp.com/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+  }).then((response) => response.json())
+    .then((data) => {
+      console.log("Login response: ", data);
+    if (data.user) {
+      localStorage.setItem("user", JSON.stringify(data.user));
+      localStorage.setItem("token", data.token);
+      onLoggedIn(data.user, data.token);  //Here you write depending on if with tokens or a simple if (response.ok) { onLoggedIn(username);
+        
+    } else {
+      alert("Login failed");
+      }
+    });
+  };
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input 
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+         />
+      </label>
+      <label>
+        Password:
+        <input 
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+         />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,16 +3,51 @@ import { Container, Row, Col } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-    fetch('https://letflix-0d183cd4a94e.herokuapp.com/movies')
-        .then((response) => response.json())
-        .then((data) => {
-          const moviesFromApi = data.map((movie) => ({
+    if (!token) return;
+
+    fetch('https://letflix-0d183cd4a94e.herokuapp.com/movies', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    const moviesFromApi = data.map((movie) => ({
+      _id: movie._id,
+      Title: movie.Title,
+      Description: movie.Description,
+      Genre: {
+        Name: movie.Genre.Name,
+        Description: movie.Genre.Description
+      },
+      Director: {
+        Name: movie.Director.Name,
+        Bio: movie.Director.Bio,
+        Birth: movie.Director.Birth
+      }
+    }));
+
+    setMovies(moviesFromApi);
+  })
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+    // Handle error if needed
+  });
+}, [token]);
+
+      /*     const moviesFromApi = data.map((movie) => ({
             _id: movie._id,
             Title: movie.Title,
             Description: movie.Description,
@@ -34,7 +69,21 @@ export const MainView = () => {
           console.error('Error fetching data:', error);
           // Handle error if needed
         });
-    }, []);
+    }, []); */
+
+
+    if (!user) {
+      return (
+        <>
+          <LoginView onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }} />
+        or
+        <SignupView />
+        </>
+      );
+    }
 
   if (selectedMovie) {
     return (
@@ -58,6 +107,7 @@ export const MainView = () => {
           </Col>
         ))}
       </Row>
+      <button onClick={() => { setUser(null); setToken(null) }}>Logout</button>
     </Container>
   );
 };
